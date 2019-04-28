35e982ab8117f0990d4db95d182ef1ce8a89d057
reika727 <reikasuya727@gmail.com>
2019-04-29 03:38:29 +0900

    アルファベット一文字の変数を実装

--- test.sh
+++ test.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 try() {
     input=$1
-    expected=$(($input))
+    expected=$2
     ./mycc "$input" > tmp.s
     gcc -o tmp tmp.s
     ./tmp
     actual=$?
     if [ $actual = $expected ]; then
-	echo "$input = $actual"
+	echo "$input => $actual"
     else
 	echo "expected $expected, but got $actual"
 	exit 1
     fi
 }
-try '0'
-try '42'
-try '5+20-4'
-try ' 12 + 34 - 5 '
-try '5+6*7'
-try '5*(9-6)'
-try '(3+5)/2'
-try '(-3+2+2*(3-2+9)+(2+4+5)*(8/2*3)+(4+4*4)/(3+2))+(-(11-2))'
-try '+4+-2'
-try '1<2'
-try '2<=2'
-try '3>1'
-try '3>=2'
+try 'a=2;b=3;b;' 3
+try 'a=2;b=3;c=(a+a+a)/b*2;d=(c+a)*2;a=d*2;' 24
+#try '0;' 0
+#try '42;' 42
+#try '5+20-4;' 21
+#try ' 12 + 34 - 5 ;' 41
+#try '5+6*7;' 47
+#try '5*(9-6);' 15
+#try '(3+5)/2;' 4
+#try '(-3+2+2*(3-2+9)+(2+4+5)*(8/2*3)+(4+4*4)/(3+2))+(-(11-2));' 146
+#try '+4+-2;' 2
+#try '1<2;' 1
+#try '2<=2;' 1
+#try '3>1;' 1
+#try '3>=2;' 1
+#try 'a=2;' 2
+#try 'a=b=2;' 2
 echo OK
