b04bb460542605e753ccb775c562ad3d5ffd6310
reika727 <reikasuya727@gmail.com>
2019-05-17 02:10:24 +0900

    字句解析部を一部修正

--- source/token_array.cpp
+++ source/token_array.cpp
@@ -5,94 +5,37 @@ using namespace lexicon;
 token_array::token_array(const std::string&s)
 {
     for(int i=0;i<s.length();++i){
-	if(isspace(s[i]))continue;
-	else if(s[i]=='(')tv.push_back(new symbol(TK::OPARENT));
-	else if(s[i]==')')tv.push_back(new symbol(TK::CPARENT));
-	else if(s[i]=='{')tv.push_back(new symbol(TK::OBRACE));
-	else if(s[i]=='}')tv.push_back(new symbol(TK::CBRACE));
-	else if(s[i]==';')tv.push_back(new symbol(TK::SCOLON));
-	else if(s[i]==',')tv.push_back(new symbol(TK::COMMA));
-	else if(s[i]=='+'){
-	    if(i!=s.length()-1&&s[i+1]=='+'){
-		tv.push_back(new symbol(TK::PLPL));++i;
-	    }else if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::PLEQ));++i;
-	    }else{
-		tv.push_back(new symbol(TK::PLUS));
-	    }
-	}else if(s[i]=='-'){
-	    if(i!=s.length()-1&&s[i+1]=='-'){
-		tv.push_back(new symbol(TK::MIMI));++i;
-	    }else if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::MIEQ));++i;
-	    }else{
-		tv.push_back(new symbol(TK::MINUS));
-	    }
-	}else if(s[i]=='*'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::ASEQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::ASTER));
-	    }
-	}else if(s[i]=='/'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::SLEQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::SLASH));
-	    }
-	}else if(s[i]=='%'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::PEEQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::PERCENT));
-	    }
-	}else if(s[i]=='='){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::EQEQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::EQUAL));
-	    }
-	}else if(s[i]=='!'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::EXEQ));
-		++i;
-	    }else{
-		throw std::runtime_error("認識できないトークンが含まれます");
-	    }
-	}else if(s[i]=='<'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::LEEQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::LESS));
-	    }
-	}else if(s[i]=='>'){
-	    if(i!=s.length()-1&&s[i+1]=='='){
-		tv.push_back(new symbol(TK::GREQ));
-		++i;
-	    }else{
-		tv.push_back(new symbol(TK::GREATER));
-	    }
-	}else if(s.substr(i,4)=="char"&&i+4<s.length()&&isspace(s[i+4])){
-	    tv.push_back(new symbol(TK::CHAR));
-	    i+=3;
-	}else if(s.substr(i,2)=="if"&&i+2<s.length()&&(s[i+2]=='('||isspace(s[i+2]))){
-	    tv.push_back(new symbol(TK::IF));
-	    ++i;
-	}else if(s.substr(i,4)=="else"&&i+4<s.length()&&(s[i+4]=='('||isspace(s[i+4]))){
-	    tv.push_back(new symbol(TK::ELSE));
-	    i+=3;
-	}else if(s.substr(i,5)=="while"&&i+5<s.length()&&(s[i+5]=='('||isspace(s[i+5]))){
-	    tv.push_back(new symbol(TK::WHILE));
-	    i+=4;
-	}else if(s.substr(i,3)=="for"&&i+3<s.length()&&(s[i+3]=='('||isspace(s[i+3]))){
-	    tv.push_back(new symbol(TK::FOR));
-	    i+=2;
-	}else if(isdigit(s[i])){
+	     if(s.substr(i,4)=="char" &&i+4<s.length()&&              isspace(s[i+4]) &&(i+=3))tv.push_back(new symbol(TK::CHAR));
+	else if(s.substr(i,2)=="if"   &&i+2<s.length()&&(s[i+2]=='('||isspace(s[i+2]))&&(i+=1))tv.push_back(new symbol(TK::IF));
+	else if(s.substr(i,4)=="else" &&i+4<s.length()&&(s[i+4]=='('||isspace(s[i+4]))&&(i+=3))tv.push_back(new symbol(TK::ELSE));
+	else if(s.substr(i,5)=="while"&&i+5<s.length()&&(s[i+5]=='('||isspace(s[i+5]))&&(i+=4))tv.push_back(new symbol(TK::WHILE));
+	else if(s.substr(i,3)=="for"  &&i+3<s.length()&&(s[i+3]=='('||isspace(s[i+3]))&&(i+=2))tv.push_back(new symbol(TK::FOR));
+	else if(s.substr(i,2)=="++"&&++i)                                                      tv.push_back(new symbol(TK::PLPL));
+	else if(s.substr(i,2)=="--"&&++i)                                                      tv.push_back(new symbol(TK::MIMI));
+	else if(s.substr(i,2)=="+="&&++i)                                                      tv.push_back(new symbol(TK::PLEQ));
+	else if(s.substr(i,2)=="-="&&++i)                                                      tv.push_back(new symbol(TK::MIEQ));
+	else if(s.substr(i,2)=="*="&&++i)                                                      tv.push_back(new symbol(TK::ASEQ));
+	else if(s.substr(i,2)=="/="&&++i)                                                      tv.push_back(new symbol(TK::SLEQ));
+	else if(s.substr(i,2)=="%="&&++i)                                                      tv.push_back(new symbol(TK::PEEQ));
+	else if(s.substr(i,2)=="=="&&++i)                                                      tv.push_back(new symbol(TK::EQEQ));
+	else if(s.substr(i,2)=="!="&&++i)                                                      tv.push_back(new symbol(TK::EXEQ));
+	else if(s.substr(i,2)=="<="&&++i)                                                      tv.push_back(new symbol(TK::LEEQ));
+	else if(s.substr(i,2)==">="&&++i)                                                      tv.push_back(new symbol(TK::GREQ));
+	else if(s[i]=='+')                                                                     tv.push_back(new symbol(TK::PLUS));
+	else if(s[i]=='-')                                                                     tv.push_back(new symbol(TK::MINUS));
+	else if(s[i]=='*')                                                                     tv.push_back(new symbol(TK::ASTER));
+	else if(s[i]=='/')                                                                     tv.push_back(new symbol(TK::SLASH));
+	else if(s[i]=='%')                                                                     tv.push_back(new symbol(TK::PERCENT));
+	else if(s[i]=='=')                                                                     tv.push_back(new symbol(TK::EQUAL));
+	else if(s[i]=='<')                                                                     tv.push_back(new symbol(TK::LESS));
+	else if(s[i]=='>')                                                                     tv.push_back(new symbol(TK::GREATER));
+	else if(s[i]=='(')                                                                     tv.push_back(new symbol(TK::OPARENT));
+	else if(s[i]==')')                                                                     tv.push_back(new symbol(TK::CPARENT));
+	else if(s[i]=='{')                                                                     tv.push_back(new symbol(TK::OBRACE));
+	else if(s[i]=='}')                                                                     tv.push_back(new symbol(TK::CBRACE));
+	else if(s[i]==';')                                                                     tv.push_back(new symbol(TK::SCOLON));
+	else if(s[i]==',')                                                                     tv.push_back(new symbol(TK::COMMA));
+	else if(isdigit(s[i])){
 	    size_t sz;
 	    tv.push_back(new numeric(std::stoi(s.substr(i),&sz)));
 	    i+=sz-1;
@@ -101,7 +44,7 @@ token_array::token_array(const std::string&s)
 	    auto len=find_if_not(beg,s.end(),[](char c){return isalpha(c)||isdigit(c)||c=='_';})-beg;
 	    tv.push_back(new ident(s.substr(i,len)));
 	    i+=len-1;
-	}else{
+	}else if(!isspace(s[i])){
 	    throw std::runtime_error("認識できないトークンが含まれます");
 	}
     }
