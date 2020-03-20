7e165c245a8a0c7c6ac796da3db50d5fcee68f29
reika727 <reikasuya727@gmail.com>
2020-03-21 07:22:12 +0900

    main関数を改修

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include"syntax/tree.hpp"
 #include<iostream>
+#include<string>
 #include<stdexcept>
 int main(int argc,char**argv)
 {
     try{
         if(argc==1)throw std::runtime_error("引数が不足しています");
+        else if(argc>3)throw std::runtime_error("引数が多すぎます");
         std::ifstream ifs{argv[1]};
-        syntax::tree tr{std::string{std::istreambuf_iterator<char>{ifs},std::istreambuf_iterator<char>{}}};
+        std::string source_code{std::istreambuf_iterator<char>{ifs},std::istreambuf_iterator<char>{}};
+        syntax::tree tr{source_code};
         tr.check();
-        tr.to_asm(std::string{argc==2?"tmp.s":argv[2]});
+        tr.to_asm(argc==2?"tmp.s":argv[2]);
     }catch(const std::exception&e){
         std::cout<<e.what()<<std::endl;
         return EXIT_FAILURE;
