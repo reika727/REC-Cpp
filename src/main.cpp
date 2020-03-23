0fdc8293e30882cf67ecb4d6c404ece55eee6e04
reika727 <reikasuya727@gmail.com>
2020-03-24 05:31:25 +0900

    一様初期化を使っていたのをキャンセル

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ int main(int argc,char**argv)
             throw std::runtime_error("引数が不足しています");
         else if(argc>3)
             throw std::runtime_error("引数が多すぎます");
-        std::ifstream ifs{argv[1]};
-        std::string source_code{std::istreambuf_iterator<char>{ifs},std::istreambuf_iterator<char>{}};
-        syntax::tree tr{source_code};
+        std::ifstream ifs(argv[1]);
+        std::string source_code((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
+        syntax::tree tr(source_code);
         tr.check();
         tr.to_asm(argc==2?"tmp.s":argv[2]);
     }catch(const std::exception&e){
