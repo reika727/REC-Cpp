5b9a792135e57b044439e62a5d30e099589232b7
reika727 <reikasuya727@gmail.com>
2020-04-01 02:54:57 +0900

    treeクラスを廃止

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
-#include"syntax/tree.hpp"
+#include"lexicon/token_array.hpp"
+#include"syntax/node.hpp"
+#include"code/generator.hpp"
 #include<fstream>
 #include<iostream>
 #include<stdexcept>
@@ -11,9 +13,10 @@ int main(int argc,char**argv)
         else if(argc>3)
             throw std::runtime_error("引数が多すぎます");
         std::ifstream ifs(argv[1]);
-        std::string src((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>());
-        syntax::tree tr(src);
-        std::ofstream(argc==2?"tmp.s":argv[2])<<tr.get_assembly();
+        lexicon::token_array ta(std::string((std::istreambuf_iterator<char>(ifs)),std::istreambuf_iterator<char>()));
+        code::generator gen;
+        while(!ta.is_all_read())syntax::define_function::get(ta)->to_asm(gen);
+        std::ofstream(argc==2?"tmp.s":argv[2])<<gen.get_asm();
     }catch(const std::exception&e){
         std::cerr<<"\033[1;31m"<<e.what()<<"\033[m"<<std::endl;
         return EXIT_FAILURE;
